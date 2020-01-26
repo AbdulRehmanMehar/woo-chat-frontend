ddf5deeeb0151387e06a6fac619996a7f8a7ef1f
Abdul Rehman <mehars.6925@gmail.com>
2020-01-26 18:57:58 +0500

    updates page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'woochatfrontend';
+
+  public constructor(
+    private titleService: Title,
+    router: Router
+  ) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        let title = this.getTitle(
+          router.routerState,
+          router.routerState.root
+        ).join('-');
+        titleService.setTitle(title);
+      }
+    });
+  }
+
+  getTitle(state, parent) {
+    let data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+
+    if (state && parent) {
+      data.push(...this.getTitle(state, state.firstChild(parent)));
+    }
+    return data;
+  }
 }
