ece5db9f24ef19b51cf8a796168337e65cda0006
Abdul Rehman <mehars.6925@gmail.com>
2020-01-26 19:00:51 +0500

    added routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,69 @@
+import { MessengerComponent } from './messenger/messenger.component';
+import { ChatComponent } from './chat/chat.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      title: 'Home - WooChat'
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login - WooChat'
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register - WooChat'
+    }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'prefix',
+        redirectTo: 'create-contact'
+      },
+      {
+        path: 'create-contact',
+        component: CreateContactComponent,
+        data: {
+          title: 'Create Contact - Dashboard - WooChat'
+        }
+      },
+
+    ]
+  },
+  {
+    path: 'chatroom',
+    component: ChatComponent,
+    data: {
+      title: 'Chatroom - WooChat'
+    },
+    children: [
+      {
+        path: ':username',
+        component: MessengerComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
