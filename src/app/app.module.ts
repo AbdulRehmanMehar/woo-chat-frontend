fef57a69c29155c53c31dd7715e43c9bf7cd9a1f
Abdul Rehman <mehars.6925@gmail.com>
2020-01-26 19:01:12 +0500

    components declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessengerComponent } from './messenger/messenger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    CreateContactComponent,
+    ChatComponent,
+    MessengerComponent
   ],
   imports: [
     BrowserModule,
