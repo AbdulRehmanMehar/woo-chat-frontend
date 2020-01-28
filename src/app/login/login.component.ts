adeb61329bdcf982518527ea18a9a5656937422f
Abdul Rehman <mehars.6925@gmail.com>
2020-01-28 15:52:48 +0500

    implemented login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,11 @@
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthState } from './../store/models/auth.model';
+import { LoginAction } from './../store/actions/auth.actions';
+import { AppState } from './../store/models/app-state.model';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  state$: Observable<AuthState>;
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private store$: Store<AppState>) {}
 
-  ngOnInit() {
+  private intializeForm() {
+    this.loginForm = new FormGroup({
+
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(12)
+      ]),
+    });
   }
 
+  ngOnInit() {
+    this.intializeForm();
+    this.state$ = this.store$.select(store => store.auth);
+    console.log(this.state$)
+  }
+
+  handleLogin() {
+    this.store$.dispatch(new LoginAction(this.loginForm.value));
+  }
 }
