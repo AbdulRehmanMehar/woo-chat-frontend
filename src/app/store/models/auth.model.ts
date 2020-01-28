4c4cd3f6e82f9c5bc0b9183721122dc5510263d5
Abdul Rehman <mehars.6925@gmail.com>
2020-01-28 15:43:52 +0500

    auth models for @ngrx

---
+++ src/app/store/models/auth.model.ts
@@ -0,0 +1,45 @@
+export interface User {
+  id: number;
+  name: string;
+  phone: string;
+  token: string;
+  username: string;
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface RegisterCredentials {
+  name: string;
+  username: string;
+  phone: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface AuthResponseErrors {
+  msg: string;
+  param: string;
+  location?: string;
+}
+
+export interface AuthReponseData {
+  user?: User;
+}
+
+export interface AuthResponse {
+  message: string;
+  success: boolean;
+  data?: AuthReponseData;
+  errors?: AuthResponseErrors[];
+}
+
+export interface AuthState {
+  user?: User;
+  loading: boolean;
+  authenticated: boolean;
+  loginError?: AuthResponseErrors[];
+  registerError?: AuthResponseErrors[];
+}
