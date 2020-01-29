a60516138c2cfb0e5922d9cc3334ab6eaeb5d7ed
Abdul Rehman <mehars.6925@gmail.com>
2020-01-29 11:54:10 +0500

    displayed contacts in chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,14 @@
+import { Router } from '@angular/router';
+import { Message } from './../store/models/io.model';
+import { IoService } from './../services/io.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoadContactsAction } from './../store/actions/contact.actions';
+import { map } from 'rxjs/operators';
+import { User } from './../store/models/auth.model';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+import { AppState } from '../store/models/app-state.model';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +16,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  search: FormGroup;
+  contacts$: Observable<User[]>;
 
-  constructor() { }
+  constructor(private store$: Store<AppState>, private io$: IoService, public router$: Router) { }
+
+  filterContacts() {
+    const data = this.search.get('field').value;
+    if (data) {
+      this.contacts$ = this.contacts$.pipe(
+        map(users => {
+          users = users.filter(user => user.name.includes(data) || user.username.includes(data))
+          return users;
+        })
+      );
+    }
+  }
 
   ngOnInit() {
+    this.store$.dispatch(new LoadContactsAction());
+    this.contacts$ = this.store$.select(state => state.contact.contacts);
+
+    this.search = new FormGroup({
+      field: new FormControl('')
+    });
+
+    this.io$.listen('message').subscribe((resp: Message) => {
+      let count = 1;
+      this.contacts$ = this.contacts$.pipe(
+        map((users: User[], mapIdx) => {
+          users.forEach((user, idx) => {
+            if (user.id.toString() == resp.sender_id && count === 1) {
+              users[idx].unreadMessages = users[idx].unreadMessages || 0;
+              if ((users[idx].unreadMessages || users[idx].unreadMessages === 0)) {
+                users[idx].unreadMessages++;
+              }
+              count++;
+            }
+          });
+          return users;
+        })
+      );
+    });
   }
 
 }
