29441a3af5b50c34542eb7ac43d00240f68914d4
Abdul Rehman <mehars.6925@gmail.com>
2020-01-29 11:49:21 +0500

    contact service

---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,41 @@
+import { async } from '@angular/core/testing';
+import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { ContactResponse, AddContactFormCredentials } from '../store/models/contact.model';
+import { AppState } from '../store/models/app-state.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+
+  private BASE_URL = environment.serverAddress;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  loadContacts() {
+    return this.http.get<ContactResponse>(`${this.BASE_URL}/protected/contacts`, {
+      headers: {
+        Authorization: JSON.parse(localStorage.getItem('authState')).user.token
+      }
+    });
+  }
+
+  createContact(credentials: AddContactFormCredentials) {
+
+    return this.http.post<ContactResponse>(`${this.BASE_URL}/protected/contact`, credentials, {
+      headers: {
+        Authorization: JSON.parse(localStorage.getItem('authState')).user.token
+      }
+    });
+  }
+
+}
