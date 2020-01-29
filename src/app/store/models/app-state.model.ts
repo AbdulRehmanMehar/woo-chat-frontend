6a5f12f4859cf9e3b011b8f20892376a5ebb07b0
Abdul Rehman <mehars.6925@gmail.com>
2020-01-29 11:49:55 +0500

    added contactState

--- src/app/store/models/app-state.model.ts
+++ src/app/store/models/app-state.model.ts
@@ -1,5 +1,7 @@
+import { ContactState } from './contact.model';
 import { AuthState } from './auth.model';
 
 export interface AppState {
   readonly auth: AuthState;
+  readonly contact: ContactState;
 }
