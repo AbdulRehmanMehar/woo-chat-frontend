dfa0985b5cff4ba54decd08a92825489b3c428f9
Abdul Rehman <mehars.6925@gmail.com>
2020-01-29 11:53:02 +0500

    contact form implementation

--- src/app/create-contact/create-contact.component.ts
+++ src/app/create-contact/create-contact.component.ts
@@ -1,4 +1,10 @@
+import { AddContactAction } from './../store/actions/contact.actions';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
+import { AppState } from '../store/models/app-state.model';
+import { ContactState } from '../store/models/contact.model';
 
 @Component({
   selector: 'app-create-contact',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  state$: Observable<ContactState>;
+
+  constructor(public store$: Store<AppState>){}
+
+  private intializeForm() {
+    this.contactForm = new FormGroup({
+
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(12),
+        Validators.maxLength(12)
+      ]),
+    });
+  }
 
   ngOnInit() {
+    this.intializeForm();
+    this.state$ = this.store$.select(state => state.contact);
+  }
+
+  handleSubmission() {
+    this.store$.dispatch(new AddContactAction(this.contactForm.value));
+    this.contactForm.reset();
   }
 
 }
